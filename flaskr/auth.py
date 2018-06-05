320c21af89b9dc3bf34db95e67cc287efe140d4f
Kai Brabo <kaibrabo@gmail.com>
2018-06-04 17:07:13 -0700

    ADD css to views

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -20,9 +20,9 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = 'Username is required.'
         elif not password:
-            error = 'Password is required'
+            error = 'Password is required.'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
@@ -36,7 +36,9 @@ def register():
             db.commit()
             return redirect(url_for('auth.login'))
 
+        print(error)
         flash(error)
+
     return render_template('auth/register.html')
 
 
